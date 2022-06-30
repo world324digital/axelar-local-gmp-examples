d97179633c9f2786ef5f83467f865983283d25b0
world324digital <world.digital.dev@gmail.com>
2022-06-30 08:48:10 +0300

    init

---
+++ examples/call-contract/index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const { getDefaultProvider, Contract, constants: { AddressZero } } = require('ethers');
+const { utils: { deployContract }} = require('@axelar-network/axelar-local-dev');
+
+const ExecutableSample = require('../../build/ExecutableSample.json');
+
+async function deploy(chain, wallet) {
+    console.log(`Deploying ExecutableSample for ${chain.name}.`);
+    const contract = await deployContract(wallet, ExecutableSample, [chain.gateway, chain.gasReceiver]);
+    chain.executableSample = contract.address;
+    console.log(`Deployed ExecutableSample for ${chain.name} at ${chain.executableSample}.`);
+}
+
+async function test(chains, wallet, options) {
+    const args = options.args || [];
+    const getGasPrice = options.getGasPrice;
+    for(const chain of chains) {
+        const provider = getDefaultProvider(chain.rpc);
+        chain.wallet = wallet.connect(provider);
+        chain.contract = new Contract(chain.executableSample, ExecutableSample.abi, chain.wallet);
+    }
+    const source = chains.find(chain => chain.name == (args[0] || 'Avalanche'));
+    const destination = chains.find(chain =>chain.name == (args[1] || 'Fantom'));
+    const message = args[2] || `Hello ${destination.name} from ${source.name}, it is ${new Date().toLocaleTimeString()}.`;
+    
+    async function print() {
+        console.log(`value at ${destination.name} is ${await destination.contract.value()}`)
+    }
+    function sleep(ms) {
+        return new Promise((resolve)=> {
+            setTimeout(() => {resolve()}, ms);
+        })
+    }
+
+    console.log('--- Initially ---');
+    await print();
+
+    //Set the gasLimit to 3e5 (a safe overestimate) and get the gas price.
+    const gasLimit = 3e5;
+    const gasPrice = await getGasPrice(source, destination, AddressZero);
+    
+    await (await source.contract.setRemoteValue(
+        destination.name,
+        destination.executableSample,
+        message, 
+        {value: BigInt(Math.floor(gasLimit * gasPrice))}
+    )).wait();
+    while(await destination.contract.value() != message) {
+        await sleep(2000);
+    }
+
+    console.log('--- After ---');
+    await print();
+}
+
+module.exports = {
+    deploy,
+    test,
+}
---
+++ examples/headers/index.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const { getDefaultProvider, Contract, constants: { AddressZero } } = require('ethers');
+const { utils: { deployContract }} = require('@axelar-network/axelar-local-dev');
+
+const Headers = require('../../build/Headers.json');
+const Gateway = require('../../build/IAxelarGateway.json');
+const IERC20 = require('../../build/IERC20.json');
+
+async function deploy(chain, wallet) {
+    console.log(`Deploying Headers for ${chain.name}.`);
+    const contract = await deployContract(wallet, Headers, [chain.gateway, chain.gasReceiver, 10]);
+    chain.headers = contract.address;
+    console.log(`Deployed Headers for ${chain.name} at ${chain.executableSample}.`);
+}
+async function postDeploy(chain, chains, wallet) {
+    const contract = new Contract(chain.headers, Headers.abi, wallet);
+    for(const otherChain of chains) {
+        if(chain == otherChain) continue;
+        console.log(`Linking ${chain.name} -> ${otherChain.name}.`);
+        await (await contract.addSibling(otherChain.name, otherChain.headers)).wait();
+        console.log(`Linked ${chain.name} -> ${otherChain.name}.`);
+    }
+}
+
+async function test(chains, wallet, options) {
+    const args = options.args || [];
+    const getGasPrice = options.getGasPrice;
+    for(const chain of chains) {
+        chain.provider = getDefaultProvider(chain.rpc);
+        chain.wallet = wallet.connect(chain.provider);
+        chain.contract = new Contract(chain.headers, Headers.abi, chain.wallet);
+        const gateway = new Contract(chain.gateway, Gateway.abi, chain.wallet);
+        const usdcAddress = await gateway.tokenAddresses('aUSDC');
+        chain.usdc = new Contract(usdcAddress, IERC20.abi, chain.wallet);
+    }
+    const source = chains.find(chain => chain.name == (args[0] || 'Avalanche'));
+    const destination = chains.find(chain =>chain.name == (args[1] || 'Fantom'));
+    
+    
+    function sleep(ms) {
+        return new Promise((resolve)=> {
+            setTimeout(() => {resolve()}, ms);
+        })
+    }
+
+    const gasLimit = 3e5;
+    const gasPrice = await getGasPrice(source, destination, source.usdc.address);
+
+    await (await source.usdc.approve(
+        source.contract.address,
+        BigInt(gasLimit * gasPrice),
+    )).wait();
+    
+    const tx = await (await source.contract.updateRemoteHeaders(
+        source.usdc.address,
+        [destination.name],
+        [BigInt(gasLimit * gasPrice)],
+    )).wait();
+    const hash = (await source.provider.getBlock(tx.blockNumber-1)).hash;
+    
+    while(true) {
+        try{
+            const remoteHash = await destination.contract.getHeader(source.name, 0);
+            if(remoteHash.header_ == hash && tx.blockNumber-1 == remoteHash.block_) break;
+            await sleep(2000);
+        } catch (e) {
+            await sleep(2000);
+        }
+    }
+
+    console.log('Success!');
+}
+
+module.exports = {
+    deploy,
+    postDeploy,
+    test,
+}
